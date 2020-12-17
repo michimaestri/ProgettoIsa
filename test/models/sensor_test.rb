6a9dc2eee5cd55a47ae1af8209eb2d0a19e558dd
Michele <maestri612@gmail.com>
2020-12-17 12:47:00 +0100

    sensore valido?

--- test/models/sensor_test.rb
+++ test/models/sensor_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class SensorTest < ActiveSupport::TestCase
-   def setup
-   	@sensor = Sensor.new(mac: "3D:F2:C9:A6:B3:4F",url: "b", tipo: "a",  latitudine: "asd", longitudine: "asda", unit_misura: "sad", public: false, downtime_to_alarm: "2020-12-14 15:31:53", last_firmware_update: "2020-12-14")
+	 def setup
+		@user = User.create(id: 1,nome:"Matteo",email:"matteo@gmail.com",password: "sadaasd")
+   	@sensor = Sensor.new(mac: "3D:F2:C9:A6:B3:4F",url: "b", tipo: "a",  latitudine: "asd", longitudine: "asda", unit_misura: "sad", public: false, downtime_to_alarm: "2020-12-14 15:31:53", last_firmware_update: "2020-12-14",user_id: 1)
    end	
 
 	test "Sensore valido" do
-   		assert_not @sensor.valid?	
+   		assert @sensor.valid?
 	end
 
 end
