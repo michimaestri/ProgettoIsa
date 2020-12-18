e65b3985b74306c0a856c1599a2a0c9d547239f1
Marco <marco.andreotti94@gmail.com>
2020-12-18 17:12:12 +0100

    Inizio misurazioni

--- app/models/misuration.rb
+++ app/models/misuration.rb
@@ -2,5 +2,5 @@ class Misuration < ApplicationRecord
 	validates :mac, presecence: true
 	validates :value, presecence: true
 	validates :unit, presecence: true
-	belongs_to :sensors, dependent: :destroy
+	belongs_to :sensors
 end
