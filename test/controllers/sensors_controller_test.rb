2f44ccfac6fd1e7e2a6a8aa3a28cf463634d6cca
Michele <maestri612@gmail.com>
2020-12-18 12:17:59 +0100

    aggiornata la show del singolo sensore

--- test/controllers/sensors_controller_test.rb
+++ test/controllers/sensors_controller_test.rb
@@ -17,13 +17,13 @@ class SensorsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  #test "should create sensor" do
-    #assert_difference('Sensor.count',1) do
-      #post sensors_url, params:{sensor: {mac: '3D:F2:C1:A3:C3:2F',url: 'bb', tipo: 'aa',  latitudine: 'asda', longitudine: 'asdaa', unit_misura: 'sada', public: false, downtime_to_alarm: '2020-12-14 15:32:53', last_firmware_update: '2020-13-14',user_id: 1} }
-    #end
+  test "should create sensor" do
+    assert_difference('Sensor.count',1) do
+      post sensors_url, params:{sensor: {mac: '3D:F2:C1:A3:C3:2F',url: 'bb', tipo: 'aa',  latitudine: 'asda', longitudine: 'asdaa', unit_misura: 'sada', public: false, downtime_to_alarm: '2020-12-14 15:32:53', last_firmware_update: '2020-13-14',user_id: 1} }
+    end
 
-    #assert_redirected_to sensor_url(Sensor.last)
-  #end
+    assert_redirected_to sensor_url(Sensor.last)
+  end
 
   test "should show sensor" do
     get sensor_url(@sensor)
