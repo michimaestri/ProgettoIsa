5a29b32ea34a29c43feefee595c4707678e98b6e
Michele <maestri612@gmail.com>
2020-12-29 15:13:32 +0100

    messe a posto delle cose del layout

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -2,7 +2,7 @@ class Sensor < ApplicationRecord
   belongs_to :user
   belongs_to :sensors_group
   has_many :misurations, dependent: :destroy
-  #validates :mac, presence: true,	format: { with: /\A([0-9A-F]{2}[:-]){5}([0-9A-F]{2})\z/ , message: "mac regex control not passed" }
+  validates :mac, presence: true,	format: { with: /\A([0-9A-F]{2}[:-]){5}([0-9A-F]{2})\z/ , message: "mac regex control not passed" }
   validates :latitudine, presence: true
   validates :longitudine, presence: true
   validates :unit_misura, presence: true
